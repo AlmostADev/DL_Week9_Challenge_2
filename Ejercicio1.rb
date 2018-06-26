f551a33421befc5d6cb662b74307e256779702bc
Olmos Soto Manuel Alejandro <MSOLMOS@entel.cl>
2018-06-26 11:10:34 -0400

    Add exercise files

---
+++ Ejercicio1.rb
@@ -0,0 +1,14 @@
+# El archivo casino.txt contiene las recaudaciones de 4 días de un casino separadas por
+# mesa de juego.
+# 
+# Se pide:
+# Crear una clase Table cuyo constructor reciba el nombre de la mesa y las
+# recaudaciones correspondientes de cada día.
+# Crear un método que permita leer el archivo e instanciar una mesa por línea del
+# archivo.
+# Crear métodos que permitan:
+# Conocer el mayor valor recaudado, y a que mesa y día corresponde.
+# Calcular el promedio total de lo recaudado por todas las mesas en todos los
+# Actividad 018 - Objetos
+# Ejercicio 1: Objetos y archivos.
+# días.
