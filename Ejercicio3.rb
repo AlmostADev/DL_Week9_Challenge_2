335765bb02a245b2a78bf673ddbed3a8eb71958b
Manuel Olmos <manuelalejandro.soto1@gmail.com>
2018-07-06 22:21:19 -0400

    Create three methods to achieve doing the exercises

--- Ejercicio3.rb
+++ Ejercicio3.rb
@@ -4,17 +4,28 @@ r = (1..10).to_a
 
 # Se pide:
 # Crear una clase llamada Roulette.
-
 class Roulette
+    # Crear un método de instancia llamado play que reciba como argumento un número
+    # al cual se está apostando. Luego debe tomar un valor -del array- al azar y
+    # compararlo con el número al cual se apostó.
+    def play(numero)
 
+    end
+
+    # Crear un método de instancia que:
+    # Debe almacenar el número generado al azar en un archivo llamado
+    # roulette_history.txt
+    # Si el número al cual se apostó corresponde al generado al azar entonces,
+    # debe además almacenar ese número en un archivo llamado winners.txt
+    def results_history(bet_num, random)
+
+    end
+
+    # Crear un método que lea el archivo rouletter_history.txt y retorne el valor que más ha
+    # generado la ruleta históricamente.
+    def best_shot
+    
+    end
 end
-# Crear un método de instancia llamado play que reciba como argumento un número
-# al cual se está apostando. Luego debe tomar un valor -del array- al azar y
-# compararlo con el número al cual se apostó.
-# Crear un método de instancia que:
-# Debe almacenar el número generado al azar en un archivo llamado
-# roulette_history.txt
-# Si el número al cual se apostó corresponde al generado al azar entonces,
-# debe además almacenar ese número en un archivo llamado winners.txt
-# Crear un método que lea el archivo rouletter_history.txt y retorne el valor que más ha
-# generado la ruleta históricamente.
+
+Roulette.new.play(1)
